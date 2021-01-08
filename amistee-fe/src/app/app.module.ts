e328471ba89f01fbe6a9b2910cc678b15cb28651
Sanjay Patel <sanjayp@mprglobalsolutions.com>
2021-01-08 09:30:31 +0530

    - Added version 02

--- amistee-fe/src/app/app.module.ts
+++ amistee-fe/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertModule } from 'ngx-bootstrap/alert';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { DetailComponent } from './modals/detail.component';
+import { Crm01Component } from './crm01/crm01.component';
+import { Crm02Component } from './crm02/crm02.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routing';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailComponent
+    DetailComponent,
+    Crm01Component,
+    Crm02Component
   ],
   imports: [
     BrowserModule,
@@ -25,7 +33,11 @@ import { DetailComponent } from './modals/detail.component';
     ModalModule.forRoot(),
     NgxSpinnerModule,
     BrowserAnimationsModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot(AppRoutes, {enableTracing: false}),
+    CommonModule,
+    BsDatepickerModule.forRoot(),
+    FormsModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent],
